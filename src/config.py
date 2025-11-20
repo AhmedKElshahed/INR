3e091425e9cd8db6d847dda2c02e886462470e1c
Ahmed.Elshahed <ahmed.elshahed@multioss.com>
2025-11-20 11:08:11 +0100

    Best model configurations

---
+++ src/config.py
@@ -0,0 +1,10 @@
+BEST_CONFIGS = {
+    'siren':  {'first_omega_0': 80.0, 'hidden_omega_0': 30.0, 'hidden_layers': 4},
+    'mfn':    {'input_scale': 256.0, 'alpha': 6.0, 'beta': 1.0, 'hidden_layers': 4},
+    'fourier':{'gamma': 256, 'hidden_layers': 4},
+    'gauss':  {'input_scale': 256.0, 'sigma': 1.5, 'hidden_layers': 4},
+    'wire':   {'first_omega_0': 10.0, 'hidden_omega_0': 10.0, 'sigma0': 2.5, 'hidden_layers': 4},
+    'finer':  {'frequency_bands': 6, 'hidden_layers': 4},
+    'incode': {'scale': 256, 'hidden_layers': 4},
+    'fr':     {'F': 64,'hidden_layers': 4},
+}
