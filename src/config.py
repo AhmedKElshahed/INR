f79a9d23423d68c3503df407279db7171560f9bb
Ahmed.Elshahed <ahmed.elshahed@multioss.com>
2026-01-11 09:56:11 +0100

    Update gamma value for fourier configuration

--- src/config.py
+++ src/config.py
@@ -20,7 +20,7 @@ BEST_CONFIGS = {
 BEST_CONFIGS_3D = {
     'siren':  {'first_omega_0': 50.0, 'hidden_omega_0': 30.0, 'hidden_layers': 4},
     'mfn':    {'input_scale': 10.0,  'alpha': 6.0, 'beta': 1.0, 'hidden_layers': 4},
-    'fourier':{'gamma': 20.0,'hidden_layers': 4},
+    'fourier':{'gamma': 50.0,'hidden_layers': 4},
     'gauss':  {'input_scale': 10.0, 'sigma': 1.0, 'hidden_layers': 4},
     'wire':   {'first_omega_0': 40.0, 'hidden_omega_0': 20.0, 'sigma0': 3.0, 'hidden_layers': 4},
     'finer':  {'frequency_bands': 16, 'hidden_layers': 4},
